2cd4fcbcaa90fa66db47427d092e6b36fc47a84d
WhirrFox <WhirrFox@gmail.com>
2024-01-14 13:13:17 +0100

    Add comments in Settings.h

--- Settings.h
+++ Settings.h
@@ -2,9 +2,16 @@
 #ifndef TIMER_SETTINGS_H
 #define TIMER_SETTINGS_H
 
+// Location of log file (default: "log.csv")
 #define filename "log.csv"
+
+// History: Number of entries per page (default: 3)
 #define perPage 3
+
+// Timer: Divider of break time (default: 5)
 #define timerDevider 5
+
+// Timer: Turn on sound effect on timer completion (Windows only, default: true)
 #define enableSound true
 
 #endif //TIMER_SETTINGS_H
